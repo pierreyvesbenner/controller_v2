320a70b76d15c91e3801b472fd1109d1792e749c
pierreyvesbenner <75337276+pierreyvesbenner@users.noreply.github.com>
2020-12-05 11:29:34 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 radio.setGroup(1)
+basic.showString("Hello!")
+basic.clearScreen()
 basic.forever(function () {
     if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Up)) {
-        radio.sendNumber(1)
+        radio.sendNumber(8)
         basic.showLeds(`
             . . # . .
             . # # # .
@@ -9,5 +11,43 @@ basic.forever(function () {
             . . # . .
             . . # . .
             `)
+        basic.pause(100)
+        basic.clearScreen()
+    }
+    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Down)) {
+        radio.sendNumber(14)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.clearScreen()
+    }
+    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Left)) {
+        radio.sendNumber(12)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.clearScreen()
+    }
+    if (Kitronik_Game_Controller.buttonIsPressed(Kitronik_Game_Controller.ControllerButtonPins.Right)) {
+        radio.sendNumber(13)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        basic.pause(100)
+        basic.clearScreen()
     }
 })
